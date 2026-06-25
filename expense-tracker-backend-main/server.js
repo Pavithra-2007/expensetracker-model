c58670894f8df31b485c3f3ff5fc2befc6695c58
Pavithra-2007 <pavithram.24it@kongu.edu>
2026-06-25 11:40:13 +0530

    first commit

--- expense-tracker-backend-main/server.js
+++ expense-tracker-backend-main/server.js
@@ -14,7 +14,8 @@ connectDB();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173"
+      "http://localhost:5173",
+      "http://expensetracker-model.vercel.app/login"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
